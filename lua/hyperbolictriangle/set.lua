7486353ba10725334f9b9cc38652cee2e619a8e1
hyperbolic <hyperbolictriangle@proton.me>
2025-07-31 21:28:13 +0530

    telescope with keybinds

--- lua/hyperbolictriangle/set.lua
+++ lua/hyperbolictriangle/set.lua
@@ -1,4 +1,20 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = "\\"
+
 vim.o.number = true
 vim.o.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.scrolloff = 8
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+vim.opt.colorcolumn = "100"
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,14 @@
 return {
 
 	{'nvim-treesitter/nvim-treesitter', 
-		branch = 'master',
-		lazy = false,
-		build = ':TSUpdate',
-		opts = {
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust", "ocaml", "go" },
-			highlight = { enable = true },
-			indent = { enable = true },  
-		}
+	branch = 'master',
+	lazy = false,
+	build = ':TSUpdate',
+	opts = {
+		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust", "ocaml", "go" },
+		sync_install = false,
+		highlight = { enable = true, additional_vim_regex_highlighting = false },
+		indent = { enable = true },  
 	}
+}
 }
